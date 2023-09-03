b50d651c502ee8691dcc02a4a9769f763360dea7
inmiti <inmabauvel@gmail.com>
2023-09-03 18:53:08 +0200

    second commit

--- Lifecycle/ViewController.swift
+++ Lifecycle/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Mark: - Inicializers
+    init() {                                    // otro tipo inicializador xibs
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {          // inicializador para Storyboards
+        super.init(coder: coder)
+        print("🫣ViewControler init")
+    }
+    
+ // Mark: - Lifecycle
+    
+    override func loadView() {
+        super.loadView()
+        print("😒 ViewController loadView")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("😏 ViewController viewDidLoad")
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("🤭 ViewController vieWillAppear")
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("🤗 ViewController viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("🙀 ViewController viewWillDisappear")
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("😿 ViewController viewDidDisappear")
+    }
+    
+    deinit {
+        print("😡❌ ViewController deinit")
+    }
+    
+    // MARK: - Actions
+    @IBAction func didTapButton(_ sender: Any) {
+        let viewController = XIBViewController(text: "👋🏼 hellooo!!!")       //Inicializar el viewcontroller de ese xib, con inic, vacio.
+        navigationController?.show(viewController, sender: nil)       // Muestra la vista a la queremos navegar.
+        
+    }
+    
 }
+
+
+
+
 
--- Lifecycle/XIBViewController.swift
+++ Lifecycle/XIBViewController.swift
@@ -8,13 +8,37 @@
 import UIKit
 
 class XIBViewController: UIViewController {
-
+    
+    // MARK: Outlets:
+    
+    @IBOutlet weak var label: UILabel!
+    
+    // MARK: - Model:
+    private let text: String
+    
+    // MARK: - Initializers:
+    
+    init(text: String) {
+        self.text = text
+        super.init(nibName: nil, bundle: nil)
+        print("🙄 XIBViewController INIT")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = text  // el texto de nuestro label ahora es el texto pasado con el inicializador de la clase. 
 
         // Do any additional setup after loading the view.
     }
-
+    
+    deinit {
+        print("❌ XIBViewController deinit")
+    }
 
     /*
     // MARK: - Navigation
